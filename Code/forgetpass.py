ae3f1cc50fab15cc3f522178f0e9d7e4001c6e3a
PhanPhouth <phanphouth68@gmail.com>
2024-12-03 15:02:13 +0700

    add code

--- Code/forgetpass.py
+++
@@ -1,3 +0,0 @@
-class Hi:
-    def __init__(self) -> None:
-        pass
---
+++ Code/registration.py
@@ -0,0 +1,8 @@
+def Registration(self,username,phone_number,password):
+    self.username = username
+    self.__phone_number = phone_number
+    self.__password = password
+def check_password(self):
+    if(len(self.__password) > 8):
+        if(self.__password is in ):
+            
