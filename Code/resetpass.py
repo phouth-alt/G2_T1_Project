fa0b7961ad8e0cce0d8d4c944d5dc5ef9dfc2855
PhanPhouth <phanphouth68@gmail.com>
2024-12-05 08:59:29 +0700

    add

---
+++ Code/resetpass.py
@@ -0,0 +1,10 @@
+import hashlib
+class ResetPass:
+    def __init__(self, username,password):
+        self.username = username
+        self.__password = password
+    def encrypt_password(self, password):
+        return hashlib.sha256(password.encode()).hexdigest()
+    def resetpass(self):
+        with open("data.txt", 'r') as file:
+            
