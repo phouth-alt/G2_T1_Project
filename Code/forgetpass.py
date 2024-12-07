0b2be412e22d0269969f263b1e2dff2b0b796811
PhanPhouth <phanphouth68@gmail.com>
2024-12-07 23:49:29 +0700

    a

---
+++ Code/forgetpass.py
@@ -0,0 +1,1 @@
+def forget_pass(phone):
--- Code/main.py
+++ Code/main.py
@@ -4,37 +4,39 @@ class AuthenticationSystem:
         self.__password = password #password hash and store in dictionary after format to file
         self.__phone = phone #where we should store this one
     def Options(self):
-        print("============================================")
-        print("===========Authentication System============")
-        print("============================================")
-        print("1.Login")
-        print("2.Register")
-        print("3.Reset Password")
-        print("4.ForgetPassword")
-        print("5.Exit program")
-        print("============================================")
-        print("=============Enter an option================")
-        input_options = input("")
         while True:
+            print("============================================")
+            print("===========Authentication System============")
+            print("============================================")
+            print("1.Login")
+            print("2.Register")
+            print("3.Reset Password")
+            print("4.ForgetPassword")
+            print("5.Exit program")
+            print("============================================")
+            print("=============Enter an option================")
+            print("--------------------------------------------")
+            input_options = input("")
             if input_options == '1':
                 print("Didn't have action yet")
-                break
+                print("\n")
             elif input_options == '2':
                 print("Didn't have action yet")
-                break
+                print("\n")
+
             elif input_options == '3':
                 print("Didn't have action yet")
-                break
+                print("\n")
+
             elif input_options == '4':
                 print("Didn't have action yet")
-                break
+                print("\n")
+
             elif input_options == '5':
-                print("Exiting program...")
+                print("Exiting program successfuly")
                 break
             else:
-                print("invalid option, please choose options again")
-                break
-
+                print("invalid option, please choose options again\n")
 
 
 user1 = AuthenticationSystem("phouth","iesls03",829372344)
