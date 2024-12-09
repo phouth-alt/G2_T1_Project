3ca6f0371715dbbaa1a9136cabaef7264c6a32ac
PhanPhouth <phanphouth68@gmail.com>
2024-12-09 15:00:34 +0700

    add

--- Code/login.py
+++ Code/login.py
@@ -1,18 +1,11 @@
 from encrypt_pass import encrypt_password
+from check_username import check_password
 def login(username,password):
         try:
-            file_path = "D:\G2_T1_Project\Data\data.txt"
-            en_password = encrypt_password(password)
-            with open(file_path, "r") as file:
-                for line in file:
-                    parts = line.strip().split("\t\t")
-                    if len(parts) > 1:
-                        if parts[0] == username and parts[2] == en_password:
-                              print("Login Successful")
-                              return True
-                    else:
-                        print("Login Unsuccessful")
-                        return False
+            if check_password(username,password):
+                 print("Login successfully")
+            else:
+                 print("please try again")
         except FileNotFoundError:
             print("Error file is not found.")
             return False
