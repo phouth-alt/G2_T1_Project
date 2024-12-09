03df5ddfefef11ff3b0e0f6cd6452e9208230026
PhanPhouth <phanphouth68@gmail.com>
2024-12-09 13:14:16 +0700

    add

--- Code/resetpass.py
+++ Code/resetpass.py
@@ -4,12 +4,21 @@ from check_username import check_user
 def reset_pass(username,password):
     try:
         print(check_user(username,password))
+        if check_user(username,password):
+            password = input("New password: ")
+            password = encrypt_password(password)
+        else:
+            print("Please try again")
+            return
+        file_path = "D:\G2_T1_Project\Data\data.txt"
+        with open(file_path, 'r') as file:
+            for line in file:
+                parts = line.strip().split("\t\t")
+                if len(parts) > 2:
+                    pass
     except:
         pass
-    if check_user(username,password):
-        password = input("New password: ")
-    else:
-        print("Please try again")
+    
 
 
 reset_pass("Phan Phouth","UareMYfarVoritH00@")
