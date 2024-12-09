c6b387f987727cd1bba5034f568acb7412971743
Serey <serey.ok20@gmail.com>
2024-12-09 13:14:35 +0700

    s

--- Code/login.py
+++ Code/login.py
@@ -6,23 +6,29 @@ class Login:
     def encrypt_password(self, password):
         return hashlib.sha256(password.encode()).hexdigest()
     def check_password(self):
+        max_attempt = 3
+        attempt = 0
         try:
-            with open("data.txt", "r") as file:
+            with open("D:/Coding beginner/G2_T1_Project/Data/data.txt", "r") as file:
                 stored_data = f"{self.username} : {self.__password}"
-                for line in file:
-                    if line.strip() == stored_data:
-                        print("Login Successful")
-                        return True
-                    else:
-                        print("Login Unsuccessful")
-                        return False
+                while attempt < max_attempt:
+                    for line in file:
+                        if line.strip() == stored_data:
+                            print("Login Successful")
+                            return True
+                    attempt += 1
+                    print(f"Login Unsuccessful. Attempt remain: {max_attempt - attempt}")
+                else:
+                    print("No more attempts for a day!")
+                    return False
         except FileNotFoundError:
             print("Error file is not found.")
             return False
         except IOError:
             print("An error occurred while reading the file.")
             return False
-account_login = Login("phanphouth", "R@m@_f0rtu9e$")
+account_login = Login("rathanak_cam", "R@m@_f0rtu9e$")
 account_login.check_password()
+
         
             
