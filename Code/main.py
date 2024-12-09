aeab09634268a976bf1f9255712764e1d4cd145c
PhanPhouth <phanphouth68@gmail.com>
2024-12-09 10:07:57 +0700

    add

--- Code/main.py
+++ Code/main.py
@@ -2,16 +2,16 @@ from registration import register
 class AuthenticationSystem:
     def Options(self):
         while True:
-            print("============================================")
-            print("===========Authentication System============")
-            print("============================================")
+            print("--------------------------------------------")
+            print("-----------Authentication System------------")
+            print("--------------------------------------------")
             print("1.Login")
             print("2.Register")
             print("3.Reset Password")
             print("4.ForgetPassword")
             print("5.Exit program")
-            print("============================================")
-            print("=============Enter an option================")
+            print("--------------------------------------------")
+            print("-------------Enter an option----------------")
             print("--------------------------------------------")
             input_options = input("")
             if input_options == '1':
